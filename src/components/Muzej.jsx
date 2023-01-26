e208f76fec929e937691a3183109132e91b3cbc1
Vesna Acic <vesna.acic23@gmail.com>
2023-01-26 02:06:03 +0100

    CSS za dugmice

--- src/components/Muzej.jsx
+++ src/components/Muzej.jsx
@@ -10,12 +10,12 @@ function Muzej({muz, dodaj, mod, izbaci}) {
           <p className="card-title">{muz.naziv}</p>
           <p className="card-text">{muz.opis} <br /> Telefon: {muz.telefon} <br /> Adresa: {muz.adresa} <br /> </p>
           {mod==1 ?
-            <button className="btn btn-primary"
-            style={{marginLeft: "20px" , opacity: "80%", padding:"5px", paddingLeft: "10px", paddingRight: "10px", border: "3px solid #EEEFEB"}} 
+            <button className="btnDodaj"
+            style={{marginLeft: "20px" , opacity: "80%", padding:"10px", paddingLeft: "10px", paddingRight: "10px"}} 
             onClick={()=>dodaj(muz.id)}>Dodaj u omiljene</button>
             :
-            <button className="btn btn-danger" 
-            style={{marginLeft: "20px" , opacity: "80%", padding:"5px", paddingLeft: "10px", paddingRight: "10px", border: "3px solid #EEEFEB"}}
+            <button className="btnIzbaci" 
+            style={{marginLeft: "20px" , opacity: "80%", padding:"10px", paddingLeft: "10px", paddingRight: "10px" }}
             onClick={()=>izbaci(muz.id)}>Izbaci iz omiljenih</button>
           }
         </div>
